b58d689ad9b568048fc7c4c6016cac5ca7e465f3
220-27\dlwotkd <ysy06022@gmail.com>
2025-09-24 13:04:19 +0900

    coupon, member

---
+++ src/main/java/com/dhunters/kpop/models/member/dto/deleteMember/DeleteMemberRes.java
@@ -0,0 +1,13 @@
+package com.dhunters.kpop.models.member.dto.deleteMember;
+
+import lombok.*;
+
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeleteMemberRes {
+
+
+}
